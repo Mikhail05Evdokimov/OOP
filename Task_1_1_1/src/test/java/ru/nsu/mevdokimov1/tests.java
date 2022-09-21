a707f5bc02b88b338539ebabcf029c172137cdc3
Mikhail <evdokimm05@gmail.com>
2022-09-21 21:50:25 +0700

    more tests

--- Task_1_1_1/src/test/java/ru/nsu/mevdokimov1/tests.java
+++ Task_1_1_1/src/test/java/ru/nsu/mevdokimov1/tests.java
@@ -1,8 +1,8 @@
 package ru.nsu.mevdokimov1;
 
-import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import java.util.Random;
+//import java.util.Random;
 
 /**
  * Class with tests for my project.
@@ -23,28 +23,45 @@ public class tests {
                 assert arr[i] == arr1[i];
     }
 
-   /*     {   //snd test
-            int[] arr = {1, 2, 3, 4, 5, 6};
-            int[] arr1 = {1, 2, 3, 4, 5, 6};
-            heapsort ob = new heapsort();
-            ob.sort(arr1);
-            for (int i = 0; i < 6; i++)
-                assert arr[i] == arr1[i];
-        }
-        {   //trd test
-            int[] arr1 = {1};
-            heapsort ob = new heapsort();
-            ob.sort(arr1);
-            assert arr1[0] == 1;
-        }
-        {   //4th test
-            int[] arr = {0, 0, 0, 0, 0, 0};
-            int[] arr1 = {0, 0, 0, 0, 0, 0};
-            heapsort ob = new heapsort();
-            ob.sort(arr1);
-            for (int i = 0; i < 6; i++)
-                assert arr[i] == arr1[i];
-        }
+    /**
+     *Test number two with sorted list of numbers.
+     */
+    @Test
+    public void secondTest()
+    {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int[] arr1 = {1, 2, 3, 4, 5, 6};
+        heapsort ob = new heapsort();
+        ob.sort(arr1);
+        for (int i = 0; i < 6; i++)
+            assert arr[i] == arr1[i];
     }
-*/
+
+    /**
+     *Test number three with one element.
+     */
+    @Test
+    public void thirdTest()
+    {
+        int[] arr1 = {1};
+        heapsort ob = new heapsort();
+        ob.sort(arr1);
+        assert arr1[0] == 1;
+    }
+    /**
+     *Test number four with random elements.
+     */
+ /*   @Test
+    public void fourthTest()
+    {
+        int[] arr1 = {};
+        arr1[0] = randomNumber.nextInt(200);
+        arr1[1] = randomNumber.nextInt(200);
+        arr1[2] = randomNumber.nextInt(200);
+        arr1[3] = randomNumber.nextInt(200);
+        arr1[4] = randomNumber.nextInt(200);
+        heapsort ob = new heapsort();
+        ob.sort(arr1);
+        assert arr1[0] == 1;
+    }*/
 }
