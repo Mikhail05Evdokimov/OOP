7c5b0a43402203178bc605b383ff5ec76dfb30e4
Mikhail05Evdokimov <91541224+Mikhail05Evdokimov@users.noreply.github.com>
2022-10-01 20:50:21 +0700

    Update and rename tests.java to Tests.java

--- Task_1_1_1/src/test/java/ru/nsu/mevdokimov1/Tests.java
+++ Task_1_1_1/src/test/java/ru/nsu/mevdokimov1/Tests.java
@@ -1,12 +1,13 @@
 package ru.nsu.mevdokimov1;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 import org.junit.jupiter.api.Test;
 
 /**
  * Class with tests for my project.
  */
-public class tests {
+public class Tests {
 
     /**
      *Test number one with common list of numbers.
@@ -16,7 +17,7 @@ public class tests {
     public void firstTest() {
             int[] arr = {1, 2, 3, 4, 5, 6};
             int[] arr1 = {5, 4, 6, 1, 2, 3};
-            heapsort ob = new heapsort();
+            Heapsort ob = new Heapsort();
             ob.sort(arr1);
             assertArrayEquals(arr1, arr);
     }
@@ -29,7 +30,7 @@ public class tests {
     public void secondTest() {
         int[] arr = {1, 2, 3, 4, 5, 6};
         int[] arr1 = {1, 2, 3, 4, 5, 6};
-        heapsort ob = new heapsort();
+        Heapsort ob = new Heapsort();
         ob.sort(arr1);
         assertArrayEquals(arr1, arr);
     }
@@ -42,7 +43,7 @@ public class tests {
     public void thirdTest() {
         int[] arr1 = {1};
         int[] arr = {1};
-        heapsort ob = new heapsort();
+        Heapsort ob = new Heapsort();
         ob.sort(arr1);
         assertArrayEquals(arr1, arr);
     }
@@ -54,7 +55,7 @@ public class tests {
     public void fourthTest() {
         int[] arr1 = {};
         int[] arr = {};
-        heapsort ob = new heapsort();
+        Heapsort ob = new Heapsort();
         ob.sort(arr1);
         assertArrayEquals(arr1, arr);
     }
