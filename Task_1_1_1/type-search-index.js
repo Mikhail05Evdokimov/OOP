6013057da2384f0614798748270caf37c42eb4b3
Mikhail05Evdokimov <Mikhail05Evdokimov@users.noreply.github.com>
2022-10-01 13:50:59 +0000

    Deploying to gh-pages from @ Mikhail05Evdokimov/OOP@bb96a6f63cd6e192c2deade8d6ed6345fafefc85 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.mevdokimov1","l":"heapsort"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.mevdokimov1","l":"Heapsort"}]
