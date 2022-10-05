24fb8fec11b36cfecd19473754b9879f4298a37d
Mikhail05Evdokimov <91541224+Mikhail05Evdokimov@users.noreply.github.com>
2022-10-05 20:38:05 +0700

    Update script.bat with java...jar

--- Task_1_1_1/script.bat
+++ Task_1_1_1/script.bat
@@ -1,6 +1,7 @@
 javadoc -d .\make\ -sourcepath .\src\main\java -subpackages ru.nsu.mevdokimov1
 
-javac -d .\make\bin\ -sourcepath .\src\main\java .\src\main\java\ru\nsu\mevdokimov1\heapsort.java
+javac -d .\make\bin\ -sourcepath .\src\main\java .\src\main\java\ru\nsu\mevdokimov1\Heapsort.java
 
 mkdir .\make\jar
 jar cf .\make\jar\heapsort.jar -C .\make\bin .
+java -classpath .\make\jar\Heapsort.jar ru.nsu.mevdokimov1.Heapsort
