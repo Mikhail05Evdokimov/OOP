8466920f81784472de9efb43b1a28a593dc5659d
Mikhail <evdokimm05@gmail.com>
2022-10-12 09:49:46 +0700

    more tests

--- Task_1_2_1/src/test/java/ru/nsu/mevdokimov1/MyTests.java
+++ Task_1_2_1/src/test/java/ru/nsu/mevdokimov1/MyTests.java
@@ -1,7 +1,6 @@
 package ru.nsu.mevdokimov1;
 
 import org.junit.jupiter.api.Assertions;
-
 import org.junit.jupiter.api.Test;
 
 /**
@@ -13,21 +12,21 @@ public class MyTests {
      * unit test with push integer.
      */
     @Test
-    public void pushUnitTest(){
+    public void pushTest() {
         MyStack<Integer> stack = new MyStack<>();
         stack.CreateStack();
         stack.push(1);
         MyStack<Integer> arr = new MyStack<>();
         arr.CreateStack();
         arr.push(1);
-        Assertions.assertEquals(arr,stack);
+        Assertions.assertEquals(arr, stack);
     }
 
     /**
      * unit test with count().
      */
     @Test
-    public void countUnitTest(){
+    public void countTest() {
         MyStack<Integer> stack = new MyStack<>();
         stack.CreateStack();
         stack.push(1);
@@ -39,7 +38,7 @@ public class MyTests {
      * unit test with push_stack(Integers).
      */
     @Test
-    public void pushStackUnitTest(){
+    public void pushStackTest() {
         MyStack<Integer> stack = new MyStack<>();
         stack.CreateStack();
         MyStack<Integer> arr1 = new MyStack<>();
@@ -53,10 +52,10 @@ public class MyTests {
     }
 
     /**
-     * unit test with pop(Integers).
+     * unit test with pop integer.
      */
     @Test
-    public void popUnitTest(){
+    public void popTest() {
         MyStack<Integer> stack = new MyStack<>();
         stack.CreateStack();
         MyStack<Integer> arr1 = new MyStack<>();
@@ -70,65 +69,56 @@ public class MyTests {
         Assertions.assertEquals(stack, arr1);
     }
 
- /*   public void IntTest() {
-        MyStack<Integer> Stack = new MyStack<>();
-        Stack.CreateStack();
-        Stack.push(1);
-        Stack.push(2);
-        Integer[] arr = {3, 4};
-        Stack.pushStack(arr);
-        Integer a = Stack.pop();
-        Assertions.assertEquals(a,4);
-        a = Stack.count();
-        Assertions.assertEquals(a,3);
-        Stack.popStack(2);
-        /*Assertions.assertEquals(Stack.stack[0],1);
-        Assertions.assertNull(Stack.stack[1]);
-        Assertions.assertNull(Stack.stack[2]);
-        Assertions.assertNull(Stack.stack[3]);
-    }*/
-/*
     /**
-     * second test with stack overflow.
+     * unit test with popStack(Integers).
      */
-    /*@Test
-    public void OverflowTest() {
-        MyStack<Integer> Stack = new MyStack<>();
-        Stack.CreateStack();
-        Stack.push(1);
-        Stack.push(2);
-        Integer[] arr = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-        Stack.pushStack(arr);
-        Integer a = Stack.pop();
-        Assertions.assertEquals(a,13);
-        a = Stack.count();
-        Assertions.assertEquals(a,12);
-        Stack.popStack(5);
-       /* for(int i = Stack.cnt; i < 12; i++){
-            Assertions.assertNull(Stack.stack[i]);
-        }
-        a = Stack.count();
-        Assertions.assertEquals(a,7);*/
- /*   }
+    @Test
+    public void popStackTest() {
+        MyStack<Integer> stack = new MyStack<>();
+        stack.CreateStack();
+        MyStack<Integer> arr1 = new MyStack<>();
+        arr1.CreateStack();
+        Integer[] arr = {1, 2, 3};
+        stack.pushStack(arr);
+        arr1.push(1);
+        stack.popStack(2);
+        Assertions.assertEquals(stack, arr1);
+    }
 
     /**
-     * third test with empty stack.
+     * pop return test.
      */
- /*   @Test
-    public void EmptyTest() {
-        MyStack<Integer> Stack = new MyStack<>();
-        Stack.CreateStack();
-        Stack.push(1);
-        Integer a = Stack.pop();
-        Assertions.assertEquals(a,1);
-        a = Stack.count();
-        Assertions.assertEquals(a,0);
-        Stack.popStack(3);
-      /*  Integer[] arr = {};
-        assertArrayEquals(Stack.stack, arr);*/
- /*   }
+    @Test
+    public void popReturnTest() {
+        MyStack<Integer> stack = new MyStack<>();
+        stack.CreateStack();
+        MyStack<Integer> arr1 = new MyStack<>();
+        arr1.CreateStack();
+        stack.push(1);
+        stack.push(1);
+        Integer a = stack.pop();
+        arr1.push(a);
+        Assertions.assertEquals(stack, arr1);
+    }
 
     /**
+     *  test with empty stack and extra pop.
+     */
+    @Test
+    public void emptyTest() {
+        MyStack<Integer> stack = new MyStack<>();
+        stack.CreateStack();
+        MyStack<Integer> arr1 = new MyStack<>();
+        arr1.CreateStack();
+        stack.push(1);
+        arr1.push(1);
+        stack.pop();
+        stack.pop();
+        arr1.pop();
+        Assertions.assertEquals(stack, arr1);
+    }
+
+ /*   /**
      * fourth test with string stack.
      */
  /*   @Test
