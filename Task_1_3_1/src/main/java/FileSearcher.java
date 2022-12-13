dfde5c5ec1a0c700272c4704060fc8d2b06ac23d
Mikhail <evdokimm05@gmail.com>
2022-12-13 10:55:34 +0700

    first commit

---
+++ Task_1_3_1/src/main/java/FileSearcher.java
@@ -0,0 +1,66 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class FileSearcher for searching the substring into txt file.
+ */
+public class FileSearcher {
+
+    /**
+     * Method for searching the substring into txt file.
+     *
+     * @param filename - name of file for searching.
+     * @param string - substring to search.
+     * @return arr - array list with indexes of found substrings.
+     */
+    public static List<Long> search(InputStream filename, String string) {
+        List<Long> ansArray = new ArrayList<>();
+        List<Character> buffer = new ArrayList<>();
+        long i = 0;
+        int findCnt = 0;
+        try(Reader reader = new InputStreamReader(filename, StandardCharsets.UTF_8)) {
+            int c;
+            while((c = reader.read()) != -1){
+                i++;
+                if (c == string.charAt(findCnt)) {
+                    buffer.add((char) c);
+                    findCnt++;
+                    if (findCnt == string.length()) {
+                        ansArray.add(i - findCnt);
+                        findCnt = 0;
+                        buffer.clear();
+                    }
+                }
+                else {
+                    if(findCnt != 0) {
+                        buffer.add((char) c);
+                        buffer.remove(0);
+                        findCnt = bufferSearch(buffer, string);
+                    }
+                }
+            }
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return ansArray;
+    }
+
+    private static int bufferSearch(List<Character> buffer, String string) {
+        int findCnt = 0;
+        for (Character character : buffer) {
+            if (character == string.charAt(findCnt)) {
+                findCnt++;
+            }
+            else {
+                findCnt = 0;
+            }
+        }
+        return findCnt;
+    }
+}
---
+++ Task_1_3_1/src/test/java/SearchTests.java
@@ -0,0 +1,83 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+/**
+ * Class with tests for my searcher.
+ */
+public class SearchTests {
+
+    @Test
+    public void simpleTest() throws IOException {
+        ArrayList<Long> arr = new ArrayList<>();
+        arr.add(6L);
+        try (InputStream file = getClass().getClassLoader().getResourceAsStream("input1.txt")) {
+            Assertions.assertEquals(FileSearcher.search(file, "world"), arr);
+        }
+    }
+
+    @Test
+    public void noSubstringTest() throws IOException {
+        ArrayList<Long> arr = new ArrayList<>();
+        try (InputStream file = getClass().getClassLoader().getResourceAsStream("input2.txt")) {
+            Assertions.assertEquals(FileSearcher.search(file, "world"), arr);
+        }
+    }
+
+    @Test
+    public void atTheBeginningTest() throws IOException {
+        ArrayList<Long> arr = new ArrayList<>();
+        arr.add(0L);
+        try (InputStream file = getClass().getClassLoader().getResourceAsStream("input3.txt")) {
+            Assertions.assertEquals(FileSearcher.search(file, "world"), arr);
+        }
+    }
+
+    @Test
+    public void someSubstringsTest() throws IOException {
+        ArrayList<Long> arr = new ArrayList<>();
+        arr.add(6L);
+        arr.add(16L);
+        try (InputStream file = getClass().getClassLoader().getResourceAsStream("input4.txt")) {
+            Assertions.assertEquals(FileSearcher.search(file, "world"), arr);
+        }
+    }
+
+    @Test
+    public void russianTest() throws IOException {
+        ArrayList<Long> arr = new ArrayList<>();
+        arr.add(7L);
+        try (InputStream file = getClass().getClassLoader().getResourceAsStream("input5.txt")) {
+            Assertions.assertEquals(FileSearcher.search(file, "мир"), arr);
+        }
+    }
+
+    @Test
+    public void myTest() throws IOException {
+        ArrayList<Long> arr = new ArrayList<>();
+        try (InputStream file = getClass().getClassLoader().getResourceAsStream("input6.txt")) {
+            Assertions.assertEquals(FileSearcher.search(file, "world"), arr);
+        }
+    }
+
+    @Test
+    public void vladTest() throws IOException {
+        ArrayList<Long> arr = new ArrayList<>();
+        arr.add(2L);
+        try (InputStream file = getClass().getClassLoader().getResourceAsStream("input7.txt")) {
+            Assertions.assertEquals(FileSearcher.search(file, "ggi"), arr);
+        }
+    }
+
+    @Test
+    public void doubleLetterTest() throws IOException {
+        ArrayList<Long> arr = new ArrayList<>();
+        arr.add(1L);
+        try (InputStream file = getClass().getClassLoader().getResourceAsStream("input8.txt")) {
+            Assertions.assertEquals(FileSearcher.search(file, "world"), arr);
+        }
+    }
+}
