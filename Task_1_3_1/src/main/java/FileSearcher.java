7f8dbd5238346076f2e7227788a827f6cf66c615
Mikhail <evdokimm05@gmail.com>
2022-12-13 11:19:09 +0700

    buffer reader for HUGE files

--- Task_1_3_1/src/main/java/FileSearcher.java
+++ Task_1_3_1/src/main/java/FileSearcher.java
@@ -1,7 +1,4 @@
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,25 +20,29 @@ public class FileSearcher {
         List<Character> buffer = new ArrayList<>();
         long i = 0;
         int findCnt = 0;
-        try (Reader reader = new InputStreamReader(filename, StandardCharsets.UTF_8)) {
-            int c;
-            while ((c = reader.read()) != -1) {
-                i++;
-                if (c == string.charAt(findCnt)) {
-                    buffer.add((char) c);
-                    findCnt++;
-                    if (findCnt == string.length()) {
-                        ansArray.add(i - findCnt);
-                        findCnt = 0;
-                        buffer.clear();
-                    }
-                } else {
-                    if (findCnt != 0) {
+        try (Reader badReader = new InputStreamReader(filename, StandardCharsets.UTF_8)) {
+            try (Reader reader = new BufferedReader(badReader)) {
+                int c;
+                while ((c = reader.read()) != -1) {
+                    i++;
+                    if (c == string.charAt(findCnt)) {
                         buffer.add((char) c);
-                        buffer.remove(0);
-                        findCnt = bufferSearch(buffer, string);
+                        findCnt++;
+                        if (findCnt == string.length()) {
+                            ansArray.add(i - findCnt);
+                            findCnt = 0;
+                            buffer.clear();
+                        }
+                    } else {
+                        if (findCnt != 0) {
+                            buffer.add((char) c);
+                            buffer.remove(0);
+                            findCnt = bufferSearch(buffer, string);
+                        }
                     }
                 }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
