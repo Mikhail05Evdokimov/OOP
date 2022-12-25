d16082c106f76c0295ce6b6a6a397d80271e9119
Mikhail <evdokimm05@gmail.com>
2022-12-25 20:44:05 +0700

    main to use calculator by terminal

--- Task_1_5_1/src/main/java/Calculator.java
+++ Task_1_5_1/src/main/java/Calculator.java
@@ -67,4 +67,10 @@ public class Calculator {
         }
         return numberStack.pop();
     }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.calculator());
+    }
 }
+
