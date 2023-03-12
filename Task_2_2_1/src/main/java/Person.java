30feb5acec274767907e5857ba4c698709bbb451
Mikhail <evdokimm05@gmail.com>
2023-03-12 12:16:08 +0700

    class Person

--- Task_2_2_1/src/main/java/Person.java
+++ Task_2_2_1/src/main/java/Person.java
@@ -1,2 +1,15 @@
-public class Person {
+public class Person extends Thread{
+    int workSpeed;
+
+    @Override
+    public void run() {
+        try {
+            wait(workSpeed);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        this.workDone();
+    }
+
+    private void workDone(){}
 }
