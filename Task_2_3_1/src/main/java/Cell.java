1a29d1c6da1bdd923cfe3d2a1d01c26c109e9697
Mikhail <evdokimm05@gmail.com>
2023-04-11 09:33:28 +0700

    Cell class

--- Task_2_3_1/src/main/java/Cell.java
+++ Task_2_3_1/src/main/java/Cell.java
@@ -1,12 +1,42 @@
 public class Cell {
-    int x;
-    int y;
-    State state;
+    private final int x;
+    private final int y;
+    private State state;
+    private int snakeTimer;
     public enum State {EMPTY, SNAKE, HEAD, APPLE};
 
     public Cell(int x, int y) {
         this.x = x;
         this.y = y;
+        snakeTimer = 0;
         state = State.EMPTY;
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public void update(boolean changeTimer){
+        if (changeTimer && snakeTimer > 0){
+            snakeTimer--;
+            if (snakeTimer == 0) {
+                state = State.EMPTY;
+            }
+        }
+    }
+
+
+
 }
